d3db45f39d8ce78fb46f1f47f298760c88ef52a6
sbussisio <Sbussiso321@gmail.com>
2024-03-19 19:32:06 +0000

    add env

--- boilerbox/llama_console.py
+++ boilerbox/llama_console.py
@@ -1,7 +1,7 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 import openai
 import os
-openai.api_key = "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"
+openai.api_key = "OPENAI_API_KEY"
 
 documents = SimpleDirectoryReader("data").load_data()
 index = VectorStoreIndex.from_documents(documents)
