5c7accc9454dbf975c277248bee699123871fc92
S'Bussiso Dube <80188685+Sbussiso@users.noreply.github.com>
2024-05-22 23:42:20 +0000

    changed port

--- application.py
+++ application.py
@@ -7,4 +7,4 @@ application = app
  
 
 if __name__ == '__main__':
-    application.run(debug=True, use_reloader=False)
+    application.run(debug=True, use_reloader=False, host="0.0.0.0", port=5000)
