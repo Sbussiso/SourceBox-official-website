2d9833dfabc7bc8e63de21a1fe1c421e831ea170
S'Bussiso Dube <80188685+Sbussiso@users.noreply.github.com>
2024-05-22 23:35:46 +0000

    formated for elastic beanstalk

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from website import create_app
 
 app = create_app()
 
+application = app
+ 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    application.run(debug=True, use_reloader=False)
