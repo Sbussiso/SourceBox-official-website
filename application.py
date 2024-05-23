a0c2b7ca159897be8aca590deba71d6e653a6aab
sbussisio <Sbussiso321@gmail.com>
2024-05-23 03:33:49 +0000

    changed application to app in main

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 from website import create_app
 
 
-application = create_app()
+app = create_app()
 
 
 
  
 
 if __name__ == '__main__':
-    application.run(debug=True, use_reloader=False, host="0.0.0.0", port=80)
+    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=80)
